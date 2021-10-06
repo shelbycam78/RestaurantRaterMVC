8f19fe771fc0ad39d9839d17a20154ec0c6390f8
Shelby Simpson <shelbycam78@gmail.com>
2021-10-05 20:07:12 -0400

    model setup finished

---
+++ RestaurantRaterMVC/RestaurantRaterMVC_Walkthru/RestaurantRaterMVC_Walkthru/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterMVC_Walkthru.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+
+    }
+
+    public class RestaurnatDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ RestaurantRaterMVC/RestaurantRaterMVC_Walkthru/RestaurantRaterMVC_Walkthru/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRaterMVC_Walkthru.Startup))]
+namespace RestaurantRaterMVC_Walkthru
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
