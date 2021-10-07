c81eb0ee2338aa58cb19755586bbb785475c16d5
Shelby Simpson <shelbycam78@gmail.com>
2021-10-07 16:16:09 -0400

    edit method done

--- RestaurantRaterMVC/RestaurantRaterMVC_Walkthru/RestaurantRaterMVC_Walkthru/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/RestaurantRaterMVC_Walkthru/RestaurantRaterMVC_Walkthru/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC_Walkthru.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -65,6 +66,43 @@ namespace RestaurantRaterMVC_Walkthru.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET : Restaurant/Edit/{id}
+        //Get an id from the user
+        //Handle if the id is null
+        //Find a restaurant by that id
+        //If the restaurant doesn't exist
+        //Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        // POST : Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
